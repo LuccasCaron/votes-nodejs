ad866811256a38d755bc1b14a5ca9abaa892d932
Luccas Caron <luccascaron1@gmail.com>
2024-02-11 21:52:49 -0300

    upload

---
+++ src/http/server.ts
@@ -0,0 +1,25 @@
+import fastify from "fastify";
+import cookie from "@fastify/cookie";
+import websocket from "@fastify/websocket";
+
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
+import { pollResults } from "./wp/poll-results";
+
+const app = fastify();
+
+app.register(cookie, {
+  secret: "my-secret",
+  hook: "onRequest",
+  parseOptions: {},
+});
+
+app.register(websocket);
+
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
+app.register(pollResults);
+
+app.listen({ port: 3333 }).then(() => console.log("Server Running"));
